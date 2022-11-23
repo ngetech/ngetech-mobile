6cd892f9f80b4a70b287b31c51cbc2462f0678de
AnggaPutraa <yananggaa@gmail.com>
2022-11-24 00:40:59 +0700

    refactor: fix theme color mappig

--- lib/core/theme/base_colors.dart
+++ lib/core/theme/base_colors.dart
@@ -3,26 +3,28 @@ import 'package:flutter/material.dart';
 class BaseColors {
   static const Color black = Colors.black;
   static const Color white = Colors.white;
-  
-  static const Color blue = Color(0xFF0048FF);
-  static const MaterialColor primarySwatch = MaterialColor(
-    0xFF0048FF,
-    <int, Color>{
-      50: blue,
-      100: blue,
-      200: blue,
-      300: blue,
-      400: blue,
-      500: blue,
-      600: blue,
-      700: blue,
-      800: blue,
-      900: blue,
-    },
-  );
-  
-  static const Color charcoalDarken = Color(0x000f1112);
-  static const Color charcoalDark = Color(0x00151718);
-  static const Color charcoal = Color(0x00181a1e);
-  static const Color charcoalLight = Color(0x0055565c);
+
+  static const int _bluePrimaryValue = 0xFF3F51B5;
+  static const MaterialColor blue =
+      MaterialColor(_bluePrimaryValue, <int, Color>{
+    50: Color(0xFFE8EAF6),
+    100: Color(0xFFC5CBE9),
+    200: Color(0xFF9FA8DA),
+    300: Color(0xFF7985CB),
+    400: Color(0xFF5C6BC0),
+    500: Color(_bluePrimaryValue),
+    600: Color(0xFF394AAE),
+    700: Color(0xFF3140A5),
+    800: Color(0xFF29379D),
+    900: Color(0xFF1B278D),
+  });
+
+  static const int _charcoalPrimaryValue = 0xFF0F1112;
+  static const MaterialColor charcoal =
+      MaterialColor(_charcoalPrimaryValue, <int, Color>{
+    600: Color(0xFF55565C),
+    700: Color(0xFF181A1E),
+    800: Color(0xFF151718),
+    900: Color(_charcoalPrimaryValue),
+  });
 }
--- lib/feautures/homepage/presentation/page/home_page.dart
+++ lib/feautures/homepage/presentation/page/home_page.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:ngetech/core/theme/base_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        centerTitle: true,
-        elevation: 0,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: BaseColors.charcoal.shade900,
+        ),
+        body: Container(
+          color: BaseColors.charcoal.shade900,
+        ),
       ),
     );
   }
