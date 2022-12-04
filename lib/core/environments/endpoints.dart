5e51aae2391743d8fcfa3b0804e0e9f5354299e2
AnggaPutraa <yananggaa@gmail.com>
2022-12-04 14:57:13 +0700

    refactor: adding register, login, & logout endpoints

--- lib/core/environments/endpoints.dart
+++ lib/core/environments/endpoints.dart
@@ -1,5 +1,8 @@
 
 class EndPoints {
-  static const String baseUrl = 'ngetech.herokuapp.com';
-  static const String getPostsTech = '$baseUrl/post-tech/post-tech-json';
+  static const String baseUrl = 'https://ngetech.up.railway.app/';
+  static const String register = '$baseUrl/flutter-register-user/';
+  static const String login = '$baseUrl/flutter-login-user/';
+  static const String logout = '$baseUrl/flutter-logout-user/';
+  static const String getPostsTech = '$baseUrl/post-tech/post-tech-json/';
 }
