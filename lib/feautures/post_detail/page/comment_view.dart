042698ef31a86c58495a43609d50ebcff0bc3546
AnggaPutraa <yananggaa@gmail.com>
2022-12-09 15:16:06 +0700

    refactor: fix render overflow

--- lib/feautures/post_detail/page/comment_view.dart
+++ lib/feautures/post_detail/page/comment_view.dart
@@ -38,188 +38,143 @@ class _CommentPostTechState extends State<CommentPostTech> {
         backgroundColor: BaseColors.charcoal,
         elevation: 0,
       ),
-      body: Container(
-        decoration: const BoxDecoration(color: BaseColors.charcoal),
-        child: FutureBuilder(
-          future: dataSource.fetchComments(postId: widget.post.id!),
-          builder: (context, AsyncSnapshot<List<PostComment>> snapshot) {
-            if (snapshot.data == null) {
-              return const Center(
-                child: Text('Loading...'),
-              );
-            } else {
-              if (snapshot.data!.isEmpty) {
-                return Column(
-                  children: [
-                    Expanded(
-                        child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 12),
-                            width: MediaQuery.of(context).size.width * 0.9,
-                            height: MediaQuery.of(context).size.width,
-                            decoration: BoxDecoration(
-                              color: BaseColors.charcoal,
-                              borderRadius: BorderRadius.circular(8),
-                              image: const DecorationImage(
-                                image:
-                                    AssetImage('assets/images/yuk_comment.png'),
-                                fit: BoxFit.contain,
-                              ),
-                            ),
-                          ),
-                          const Text(
-                            'Yuk Comment !',
-                            style: TextStyle(
-                                color: BaseColors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                    )),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4,
-                        vertical: 12,
-                      ),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 28,
-                            backgroundColor: BaseColors.charcoal.shade700,
-                            child: request.isLoggedIn()
-                                ? LineIcon(
-                                    LineIcons.user,
-                                    color: BaseColors.blue,
-                                  )
-                                : LineIcon(
-                                    LineIcons.userLock,
-                                    color: BaseColors.blue,
-                                  ),
-                          ),
-                          const SizedBox(
-                            width: 12,
-                          ),
-                          Expanded(child: TextFormField()),
-                          const SizedBox(
-                            width: 12,
-                          ),
-                          Transform.rotate(
-                            angle: 5.5,
-                            child: LineIcon(
-                              LineIcons.paperPlane,
-                              size: 35,
-                              color: BaseColors.blue,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  width: double.infinity,
-                  height: double.infinity,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: ListView.builder(
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              PostComment postComment = snapshot.data![index];
-                              return Card(
-                                color: BaseColors.charcoal.shade700,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12)),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8),
-                                  child: ListTile(
-                                    leading: CircleAvatar(
-                                      radius: 28,
-                                      backgroundColor: BaseColors.charcoal,
-                                      child: request.isLoggedIn()
-                                          ? LineIcon(
-                                              LineIcons.user,
-                                              color: BaseColors.blue,
-                                            )
-                                          : LineIcon(
-                                              LineIcons.userLock,
-                                              color: BaseColors.blue,
-                                            ),
-                                    ),
-                                    title: Text.rich(TextSpan(
-                                        text: "@${postComment.username} ",
-                                        style: const TextStyle(
-                                            color: BaseColors.blue,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 14),
-                                        children: <InlineSpan>[
-                                          TextSpan(
-                                              text: postComment.comment,
-                                              style: const TextStyle(
-                                                color: BaseColors.white,
-                                                fontSize: 14,
-                                              ))
-                                        ])),
-                                    subtitle: Text(
-                                      DateFormat.yMMMMd().format(DateTime.parse(
-                                          postComment.created_on!)),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            }),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 4,
-                          vertical: 12,
-                        ),
-                        child: Row(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: dataSource.fetchComments(
+                  postId: widget.post.id!,
+                ),
+                builder: (context, AsyncSnapshot<List<PostComment>> snapshot) {
+                  if (snapshot.data == null) {
+                    return const Center(
+                      child: Text('Loading...'),
+                    );
+                  } else if (snapshot.data!.isEmpty) {
+                    return Center(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            CircleAvatar(
-                              radius: 28,
-                              backgroundColor: BaseColors.charcoal.shade700,
-                              child: request.isLoggedIn()
-                                  ? LineIcon(
-                                      LineIcons.user,
-                                      color: BaseColors.blue,
-                                    )
-                                  : LineIcon(
-                                      LineIcons.userLock,
-                                      color: BaseColors.blue,
-                                    ),
-                            ),
-                            const SizedBox(
-                              width: 12,
-                            ),
-                            Expanded(child: TextFormField()),
-                            const SizedBox(
-                              width: 12,
-                            ),
-                            Transform.rotate(
-                              angle: 5.5,
-                              child: LineIcon(
-                                LineIcons.paperPlane,
-                                size: 35,
-                                color: BaseColors.blue,
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 12),
+                              width: MediaQuery.of(context).size.width * 0.9,
+                              height: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                image: const DecorationImage(
+                                  image:
+                                      AssetImage('assets/images/yuk_comment.png'),
+                                  fit: BoxFit.contain,
+                                ),
                               ),
+                            ),
+                            const Text(
+                              'Yuk Comment !',
+                              style: TextStyle(
+                                  color: BaseColors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold),
                             )
                           ],
                         ),
                       ),
-                    ],
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        PostComment postComment = snapshot.data![index];
+                        return Card(
+                          color: BaseColors.charcoal.shade700,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                radius: 24,
+                                backgroundColor: BaseColors.charcoal,
+                                child: request.isLoggedIn()
+                                    ? LineIcon(
+                                        LineIcons.user,
+                                        color: BaseColors.blue,
+                                      )
+                                    : LineIcon(
+                                        LineIcons.userLock,
+                                        color: BaseColors.blue,
+                                      ),
+                              ),
+                              title: Text.rich(
+                                TextSpan(
+                                  text: "@${postComment.username} ",
+                                  style: const TextStyle(
+                                    color: BaseColors.blue,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                  children: <InlineSpan>[
+                                    TextSpan(
+                                      text: postComment.comment,
+                                      style: const TextStyle(
+                                        color: BaseColors.white,
+                                        fontSize: 14,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              subtitle: Text(
+                                DateFormat.yMMMMd().format(
+                                  DateTime.parse(postComment.created_on!),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 4,
+                vertical: 12,
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 28,
+                    backgroundColor: BaseColors.charcoal.shade700,
+                    child: LineIcon(
+                      LineIcons.user,
+                      color: BaseColors.blue,
+                    ),
                   ),
-                );
-              }
-            }
-          },
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(child: TextFormField()),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Transform.rotate(
+                    angle: 5.5,
+                    child: LineIcon(
+                      LineIcons.paperPlane,
+                      size: 35,
+                      color: BaseColors.blue,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
       backgroundColor: BaseColors.charcoal,
--- lib/feautures/post_detail/page/post_tech_detail.dart
+++ lib/feautures/post_detail/page/post_tech_detail.dart
@@ -54,83 +54,84 @@ class _PostTechDetailState extends State<PostTechDetail> {
     );
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: BaseColors.charcoal,
-          elevation: 0,
-        ),
-        body: Container(
-          decoration: const BoxDecoration(color: BaseColors.charcoal),
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: LineIcon(
-                  LineIcons.windowRestore,
-                  color: BaseColors.blue,
-                  size: 50,
+      appBar: AppBar(
+        backgroundColor: BaseColors.charcoal,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: LineIcon(
+                LineIcons.windowRestore,
+                color: BaseColors.blue,
+                size: 50,
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(5)),
+            Text(widget.post.title!,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                )),
+            const Padding(padding: EdgeInsets.all(5)),
+            Text.rich(TextSpan(
+                text: "Posted by ",
+                style: const TextStyle(
+                    color: BaseColors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14),
+                children: <InlineSpan>[
+                  TextSpan(
+                      text: "@${widget.post.username!}",
+                      style: const TextStyle(
+                        color: BaseColors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ))
+                ])),
+            const Padding(padding: EdgeInsets.all(5)),
+            Text(
+                "Published on ${DateFormat.yMMMMd().format(DateTime.parse(widget.post.date!))}",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: BaseColors.charcoal.shade600,
+                  fontSize: 12,
+                )),
+            const Padding(padding: EdgeInsets.all(10)),
+            Container(
+                width: 350,
+                height: 400,
+                decoration: BoxDecoration(
+                    color: BaseColors.charcoal.shade700,
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.all(13),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Text(widget.post.description!,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        )),
+                  ),
+                )),
+            const Padding(padding: EdgeInsets.all(5)),
+            SizedBox(
+              child: ListTile(
+                leading: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [likeButton, commentButton],
                 ),
               ),
-              const Padding(padding: EdgeInsets.all(5)),
-              Text(widget.post.title!,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 23,
-                  )),
-              const Padding(padding: EdgeInsets.all(5)),
-              Text.rich(TextSpan(
-                  text: "Posted by ",
-                  style: const TextStyle(
-                      color: BaseColors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14),
-                  children: <InlineSpan>[
-                    TextSpan(
-                        text: "@${widget.post.username!}",
-                        style: const TextStyle(
-                          color: BaseColors.blue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ))
-                  ])),
-              const Padding(padding: EdgeInsets.all(5)),
-              Text(
-                  "Published on ${DateFormat.yMMMMd().format(DateTime.parse(widget.post.date!))}",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: BaseColors.charcoal.shade600,
-                    fontSize: 12,
-                  )),
-              const Padding(padding: EdgeInsets.all(10)),
-              Container(
-                  width: 350,
-                  height: 400,
-                  decoration: BoxDecoration(
-                      color: BaseColors.charcoal.shade700,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(13),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Text(widget.post.description!,
-                          textAlign: TextAlign.justify,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                          )),
-                    ),
-                  )),
-              const Padding(padding: EdgeInsets.all(5)),
-              SizedBox(
-                child: ListTile(
-                  leading: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [likeButton, commentButton],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
+            )
+          ],
+        ),
+      ),
+      backgroundColor: BaseColors.charcoal,
+    );
   }
 }
